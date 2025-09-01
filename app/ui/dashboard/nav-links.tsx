0110aa116e762d4ecc7978872ff70df51f949a3f
2Zerozero <hawgae@gmail.com>
2025-09-01 19:02:31 +0900

    feat : 페이지 간 탐색

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,13 @@
-import { UserGroupIcon, HomeIcon, DocumentDuplicateIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+'use client';
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
+import { UserGroupIcon, HomeIcon, DocumentDuplicateIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+// 사이드 네비게이션에 표시할 링크들의 모음.
+// 애플리케이션 규모에 따라, 이 데이터는 데이터베이스에 저장될 수도 있다.
+
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
@@ -14,6 +19,7 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
@@ -22,7 +28,12 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href, // 현재 경로와 링크의 경로가 일치하면 활성화 스타일 적용
+              }
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
