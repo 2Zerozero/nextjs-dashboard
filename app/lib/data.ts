0435b5fd52dee8923d0af7d39df7f86b530d94b9
2Zerozero <hawgae@gmail.com>
2025-09-02 16:32:55 +0900

    feat : 정적 및 동적 랜더링

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,12 +17,12 @@ export async function fetchRevenue() {
     // → 데모 목적상 인위적으로 응답을 지연시키는 코드입니다.
     // → 실제 운영 환경에서는 이렇게 하지 마세요 :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
